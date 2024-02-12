3fdb6c2ad16276c9c19bac2460c1377aa16ecd07
404allen404 <n26120935@gs.ncku.edu.tw>
2024-02-13 01:59:59 +0800

    Add reboot feature

--- include/shell.h
+++ include/shell.h
@@ -1,7 +1,15 @@
 #ifndef __SHELL_H__
 #define __SHELL_H__
 
+#define PM_PASSWORD 0x5a000000
+#define PM_RSTC     0x3F10001c
+#define PM_WDOG     0x3F100024
+
 void shell_start();
 void parse_buf(unsigned char* buf);
+
+void set(long addr, unsigned int value);
+void reset(int tick);
+void cancel_reset();
 
 #endif
--- src/shell.c
+++ src/shell.c
@@ -2,17 +2,40 @@
 #include "mini_uart.h"
 #include "string.h"
 
+void set(long addr, unsigned int value) {
+  volatile unsigned int *point = (unsigned int*)addr;
+  *point = value;
+}
+
+void reset(int tick) {              // reboot after watchdog timer expire
+  set(PM_RSTC, PM_PASSWORD | 0x20); // full reset
+  set(PM_WDOG, PM_PASSWORD | tick); // number of watchdog tick
+}
+
+void cancel_reset() {
+  set(PM_RSTC, PM_PASSWORD | 0); // full reset
+  set(PM_WDOG, PM_PASSWORD | 0); // number of watchdog tick
+}
+
+
 void parse_buf(unsigned char *buf) {
 
-  if (str_cmp(buf, "help")) {
+  if(str_cmp(buf, "help")) {
     mini_uart_display("\nhelp   : print this help menu\n");
     mini_uart_display("hello  : print Hello World!\n");
     mini_uart_display("reboot : reboot the device\n");
     return;
   }
 
-  if (str_cmp(buf, "hello")) {
+  if(str_cmp(buf, "hello")) {
     mini_uart_display("\nHello World!\n");
+    return;
+  }
+
+  if(str_cmp(buf, "reboot")) {
+    mini_uart_display("\nReboot...\n");
+    reset(1000000000);
+    while(1);
     return;
   }
 
